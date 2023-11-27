eecc4a7bd7256ff62a13cc10e5fa667cdef3defb
Eric Coleman <eric.coleman@scicotherapylabs.com>
2023-11-27 00:11:34 -0600

    Add Swift to TreeSitter
    requires Node installation

--- init.lua
+++ init.lua
@@ -390,7 +390,7 @@ end, { desc = "Toggle Filetree" })
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'swift', 'typescript', 'vimdoc', 'vim', 'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
