9c0c6aabe75c8e55348f71986f524ef597f29eff
Eric Coleman <eric.coleman@scicotherapylabs.com>
2023-11-27 00:10:28 -0600

    Add navigation keybindings

--- init.lua
+++ init.lua
@@ -288,6 +288,12 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Navigation keymaps
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = false })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = false })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = false })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = false })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
@@ -476,7 +482,10 @@ local on_attach = function(_, bufnr)
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- [Eric Note] Let's disable this for awhile since this conflicts with our navigation efforts
+  -- Fee free to uncomment when we want to test it back out again!
+  -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
