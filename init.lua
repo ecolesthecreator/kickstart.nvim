381298c45a14ffdeef44e8368d9921678a780927
Eric Coleman <ecoleman@squareup.com>
2023-11-27 18:26:15 -0600

    Add devicons

--- init.lua
+++ init.lua
@@ -112,6 +112,10 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
+
+  -- Show devicons
+  { 'nvim-tree/nvim-web-devicons', opts = {} },
+
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
